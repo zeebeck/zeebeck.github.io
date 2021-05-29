0185e9e53bd0f045d73e27b3b591a71752bedf38
Jihun Kim <jihunkim101112@gmail.com>
2021-05-30 00:37:21 +0900

    publish it to github pages

--- src/components/head/index.jsx
+++ src/components/head/index.jsx
@@ -19,6 +19,10 @@ export function Head({ description, lang, meta, keywords, title }) {
             titleTemplate={`%s | ${data.site.siteMetadata.title}`}
             meta={[
               {
+                name: `google-site-verification`,
+                content: `1-Oue5CCjVpiCpVLHcW2844FEK6-y8pStWHdFvLH4NI`,
+              },
+              {
                 name: `description`,
                 content: metaDescription,
               },

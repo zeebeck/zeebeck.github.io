cf602ad30f2ac5a5e1775f0e32b37cf01cf47584
Jihun Kim <jihunkim101112@gmail.com>
2021-11-22 23:33:47 +0900

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-4c2616ff2747fd598908.js"
+    "url": "webpack-runtime-206ac5793a3731cc25fb.js"
   },
   {
     "url": "styles.8893625e2f772345882e.css"
@@ -45,7 +45,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "10a577232fb03610737178ecfcb01181"
+    "revision": "f392bf5032a119119193f1a7a6efe118"
   },
   {
     "url": "polyfill-c5f50f2ad13861331c8a.js"
